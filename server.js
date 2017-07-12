f5574a0ace509b6a057dab3877ac5a84bba999dc
Sagar Bansal <sagarbansal099@gmail.com>
2017-07-12 19:23:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'public/index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'public/css/styles.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
