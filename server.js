6c7c444ed66322ab70ded1bfe96d565f710ed6f3
Sagar Bansal <sagarbansal099@gmail.com>
2017-07-12 19:29:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,25 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'public/index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/public/css/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'public/css/styles.css'));
+});
+
+app.get('/ui/public/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/public/bootstrap.min.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
